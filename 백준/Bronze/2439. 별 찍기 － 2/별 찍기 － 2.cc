c81fae4af8600255f031f33e98d1b68c3cbda2a0
Soobin <114851926+soobinH@users.noreply.github.com>
2023-01-09 23:46:11 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		
+		for (int j = 0; j <n-i; j++)
+		{
+			cout << " ";
+			
+		}
+		for (int k = 0; k <i; k++)
+		{
+			cout << "*";
+		}
+
+		cout << "\n";
+	}
+	return 0;
+}
