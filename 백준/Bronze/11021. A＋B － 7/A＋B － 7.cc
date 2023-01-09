20e2b6f653c9cc774511d81e9a2a47c498609ac0
Soobin <114851926+soobinH@users.noreply.github.com>
2023-01-09 23:48:14 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int a, b, c;
+	cin >> c;
+	for (int i = 1; i <= c; i++)
+	{
+		cin >> a >> b;
+		cout << "Case #" << i << ": " << a + b << "\n";
+	}
+	return 0;
+}
