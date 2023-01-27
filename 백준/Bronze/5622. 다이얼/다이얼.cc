3702b1c808b5a2f01a386f40d6db0027c7e4be4a
Soobin <114851926+soobinH@users.noreply.github.com>
2023-01-27 09:35:08 +0900

    [Bronze II] Title: 다이얼, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+	string number;
+
+	cin >> number;
+
+	int count = 0;
+
+
+	for (int i = 0; i < number.length(); i++)
+	{
+		switch (number[i])
+		{
+		case 'A':
+		case 'B':
+		case 'C':
+			count += 3;
+			break;
+
+		case 'D':
+		case 'E':
+		case 'F':
+			count += 4;
+			break;
+
+		case 'G':
+		case 'H':
+		case 'I':
+			count += 5;
+			break;
+
+		case 'J':
+		case 'K':
+		case 'L':
+			count += 6;
+			break;
+
+		case 'M':
+		case 'N':
+		case 'O':
+			count += 7;
+			break;
+
+		case 'P':
+		case 'Q':
+		case 'R':
+		case 'S':
+			count += 8;
+			break;
+
+		case 'T':
+		case 'U':
+		case 'V':
+			count += 9;
+			break;
+
+		case 'W':
+		case 'X':
+		case 'Y':
+		case 'Z':
+			count += 10;
+			break;
+		}
+	}
+
+	cout << count << endl;
+
+	return 0;
+
+}
